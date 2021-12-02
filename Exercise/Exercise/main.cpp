914d7185ec08641e217b48cc509bbda55e46399d
dsh <dushaohe_hit@163.com>
2021-12-02 21:15:28 +0800

    Fix

--- Exercise/Exercise/main.cpp
+++ Exercise/Exercise/main.cpp
@@ -8,37 +8,6 @@
 
 #include <iostream>
 
-double calculateTax(double incomeBeforTax);
-
-int main(int argc, const char *argv[]) {
-    float salaryPerMonth = 0;
-    float shares = 0;
-    float exercisePrice = 0;
-    float currentSharePrice = 0;
-    std::cout << "input your salary: RMB/month" << std::endl;
-    std::cin >> salaryPerMonth;
-    std::cout << "input your number of shares: CDR" << std::endl;
-    std::cin >> shares;
-    std::cout << "input your exercisePrice: RMB/CDR" << std::endl;
-    std::cin >> exercisePrice;
-    std::cout << "input your currentSharePrice: RMB/CDR" << std::endl;
-    std::cin >> currentSharePrice;
-    double totalSalary = salaryPerMonth * 12;
-    std::cout << "your total salary is " << totalSalary << " RMB/year" << std::endl;
-    double salaryAfterTax = calculateTax(totalSalary);
-    std::cout << "your salary incomeAfterTax is " << salaryAfterTax << " RMB/year" << std::endl;
-    double totalProfit = (currentSharePrice - exercisePrice) * shares;
-    std::cout << "your exercise totalProfit is " << totalProfit << " RMB" << std::endl;
-    double totalIncome = totalSalary + totalProfit;
-    std::cout << "your totalIncome is " << totalIncome << " RMB" << std::endl;
-    double incomeAfterTax = calculateTax(totalIncome);
-    std::cout << "your incomeAfterTax is " << incomeAfterTax << " RMB" << std::endl;
-    double profitCost = (totalIncome - incomeAfterTax) - (totalSalary - salaryAfterTax);
-    std::cout << "your profitCost is " << profitCost << " RMB" << std::endl;
-    std::cout << "your profitCost per CDR is " << profitCost/shares << " RMB" << std::endl;
-    return 0;
-}
-
 // input: incomeBeforTax per year
 double calculateTax(double incomeBeforTax) {
     double tax1 = 36000 * 0.03;
@@ -62,5 +31,61 @@ double calculateTax(double incomeBeforTax) {
     } else if (incomeBeforTax > 960000) {
         return incomeBeforTax - tax1 - tax2 - tax3 - tax4 - tax5 - tax6 - (incomeBeforTax - 960000) * 0.45;
     }
+    return 0;
+}
+// 默认3年内完成行权
+void yearLimit(int total) {
+    int year = 3;
+    while (total >= 1000) {
+        total = total * (1 - 0.25);
+        year ++;
+    }
+    std::cout << "yearLimit is " << year << " last year shares is " << total <<std::endl;
+}
+
+int main(int argc, const char *argv[]) {
+    float salaryPerMonth = 0;
+    int shares = 0;
+    float exercisePrice = 0;
+    float currentSharePrice = 0;
+    float exchangeRate = 0;
+
+    std::cout << "input your salary: RMB/month" << std::endl;
+    std::cin >> salaryPerMonth;
+    std::cout << "input your number of shares: CDR" << std::endl;
+    std::cin >> shares;
+    std::cout << "input your exercisePrice: USD/CDR" << std::endl;
+    std::cin >> exercisePrice;
+    std::cout << "input your exchangeRate: RMB/USD" << std::endl;
+    std::cin >> exchangeRate;
+    std::cout << "input your currentSharePrice: RMB/CDR" << std::endl;
+    std::cin >> currentSharePrice;
+    exercisePrice = exercisePrice * exchangeRate;
+    
+    auto details = [=] (int num) {
+        double totalSalary = salaryPerMonth * 12;
+        std::cout << "your total salary is " << totalSalary << " RMB/year" << std::endl;
+        double salaryAfterTax = calculateTax(totalSalary);
+        std::cout << "your salary incomeAfterTax is " << salaryAfterTax << " RMB/year" << std::endl;
+        double totalProfit = (currentSharePrice - exercisePrice) * num;
+        std::cout << "your exercise totalProfit is " << totalProfit << " RMB" << std::endl;
+        double totalIncome = totalSalary + totalProfit;
+        std::cout << "your totalIncome is " << totalIncome << " RMB" << std::endl;
+        double incomeAfterTax = calculateTax(totalIncome);
+        std::cout << "your incomeAfterTax is " << incomeAfterTax << " RMB" << std::endl;
+        double shareTaxCost = (totalIncome - incomeAfterTax) - (totalSalary - salaryAfterTax);
+        std::cout << "your shareTaxCost is " << shareTaxCost << " RMB" << std::endl;
+        double costperCDR = (shareTaxCost / shares) + exercisePrice;
+        std::cout << "your costperCDR is " << costperCDR << " RMB" << std::endl;
+    };
+    std::cout << ".................一次行完................." << std::endl;
+    details(shares);
+    
+    double recommendShares = (660000 - salaryPerMonth * 12) / (currentSharePrice - exercisePrice);
+    std::cout << "recommendShares is " << recommendShares << std::endl;
+    std::cout << ".................控税30%，推荐数量................." << std::endl;
+    details(recommendShares);
+    yearLimit(shares);
+    
     return 0;
 }
